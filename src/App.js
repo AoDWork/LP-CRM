63f6c48b251b22fb6c54164c8e2061002933024d
AoDWork <aodtest07@gmail.com>
2023-02-15 13:19:03 +0200

    Stylized buttons, add hiding buttons

--- src/App.js
+++ src/App.js
@@ -12,14 +12,7 @@ function App() {
     { disabled: false, id: "4", named: "20", img: Img }
   ]);
 
-  let copied = Object.assign([], cards);
-
-  // const cards = [
-  //   { disabled: false, id: "1", named: "Blue", img: Img },
-  //   { disabled: false, id: "2", named: "8GB", img: Img },
-  //   { disabled: false, id: "3", named: "16GB", img: Img },
-  //   { disabled: false, id: "4", named: "20", img: Img }
-  // ]
+  // let copied = Object.assign([], cards);
 
   const Card = (props) => {
     return (
@@ -35,6 +28,7 @@ function App() {
           <img src={Img} alt="" />
           <input className='name' type="text" placeholder={props.named} />
         </div>
+        <button className='deleteCard'>+</button>
       </div>
     );
   }
@@ -50,41 +44,57 @@ function App() {
   let renderList = cards.map((el, ind) => <ListElement key={ind} named={el.named} />)
 
   const addCard = () => {
-    // copied.unshift({ disabled: false, id: "_", named: "____", img: Img });
     setCards([
-        { disabled: false, id: "_", named: "____", img: Img },
-        ...cards
+      { disabled: false, id: "_", named: "____", img: Img },
+      ...cards
     ]);
     console.log(cards);
   }
 
+  const handleMouseOver = e => {
+    let foo = document.querySelectorAll(".deleteCard");
+
+    for (var i = 0; i < foo.length; i++) {
+      foo[i].classList.add("reavel");
+    }
+  };
+
+  const handleMouseLeft = e => {
+    let foo = document.querySelectorAll(".deleteCard");
+
+    for (var i = 0; i < foo.length; i++) {
+      foo[i].classList.remove("reavel");
+    }
+  };
+
   return (
     <div className="App">
-      <div className='header'>
-        <div className='header__item'>Статус
-          <select className="select" name="status" id=""></select>
+      <div className='table-container' onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeft}>
+        <div className='header'>
+          <div className='header__item'>Статус
+            <select className="select" name="status" id=""></select>
+          </div>
+          <div className='header__item'>Товар
+            <select className="select" name="good" id=""></select>
+          </div>
+          <div className='header__item'>ID
+            <select className="select" name="id" id=""></select>
+          </div>
+          <div className='header__item-4'>Название
+            <select className="select" name="named" id="">
+              {renderList}
+            </select>
+          </div>
+          <div className="buttons">
+            <button className="addCardBtn" onClick={addCard}>+</button>
+            <button className="deleteChosenCards"><p>+</p></button>
+          </div>
         </div>
-        <div className='header__item'>Товар
-          <select className="select" name="good" id=""></select>
-        </div>
-        <div className='header__item'>ID
-          <select className="select" name="id" id=""></select>
-        </div>
-        <div className='header__item-4'>Название
-          <select className="select" name="named" id="">
-            {renderList}
-          </select>
-        </div>
-        <div className="buttons">
-          <button className="addCardBtn" onClick={addCard}>+</button>
-          <button className="deleteChosenCards">X</button>
+
+        <div className='body'>
+          {renderCards}
         </div>
       </div>
-
-      <div className='body'>
-        {renderCards}
-      </div>
-
     </div>
   );
 }
